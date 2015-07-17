c467a1608f63433ed5c9fe82036155e349cccc9f
Mikko Koho <mikko.koho@iki.fi>
2015-07-17 15:56:51 +0300

    Custom prefixes for namespaces

--- association.py
+++ association.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 import os
-from rdflib import Graph, RDF
+import re
+from rdflib import Graph, RDF, namespace
 import subprocess
 
 from slugify import slugify
@@ -10,6 +11,12 @@ _pwd = os.path.dirname(os.path.realpath(__file__))
 _itemset_separator = '|'
 _predicate_object_separator = '-->'
 
+_namespace_prefixes = dict(((str(uri), prefix + ':') for prefix, uri in namespace.NamespaceManager(Graph()).namespaces()))
+    # list(namespace.NamespaceManager(Graph()).namespaces()) + \
+_namespace_prefixes.update({'http://ldf.fi/schema/narc-menehtyneet1939-45/': 'sss:',
+                            'http://ldf.fi/narc-menehtyneet1939-45/': 'ss:',
+                            'http://xmlns.com/foaf/0.1/': 'foaf:',
+                   })
 
 import rdftools.helpers as h
 
@@ -19,37 +26,7 @@ import rdftools.helpers as h
 
     # triple_items = ["{s} - {p} - {o}".format(s=str(s), p=str(p), o=str(o)) for (s, p, o) in g]
 
-def frequent_items(graph):
-    '''
-    Analyse itemsets using command line implementation of FP-growth algorithm from FIM package.
-
-    :return:
-    '''
-
-    po_items = defaultdict(list)
-
-    for s, p, o in graph:
-        po_items[s].append("{p} {o}".format(s=str(s), p=str(p), o=str(o)))
-
-    with open('{pwd}/itemsets/rdf.basket'.format(pwd=_pwd), encoding='UTF-8', mode='w+') as f:
-        for s, po in po_items.items():
-            f.write(','.join(po) + "\n")
-
-    return_code = subprocess.call("fpgrowth -ts -f\",\" -s30 rdf.basket rdf.basket.freq_itemsets", shell=True)
-
-    if return_code:
-        print(return_code)
-        raise Exception('FP-growth from FIM package not found.')
-
-    return_code = subprocess.call("fpgrowth -tr -f\",\" -s20 -c95 -el -d200 rdf.basket rdf.basket.freq_rules", shell=True)
-
-    # TODO: Get lift to output
-
-    if return_code:
-        print(return_code)
-
-
-def freq_items_by_class(graph, cl, minsup1=50, minsup2=25, minconf=90, minlift=200):
+def freq_items_by_class(graph, cl, ns_prefixes=_namespace_prefixes, minsup1=50, minsup2=25, minconf=90, minlift=200):
     """
     Get frequent items by class. Uses FIM package's implementation of fpgrowth algorithm.
 
@@ -59,10 +36,18 @@ def freq_items_by_class(graph, cl, minsup1=50, minsup2=25, minconf=90, minlift=2
 
     po_items = []  # list of lists
 
+    if ns_prefixes:
+        pattern = re.compile(r'\b(' + '|'.join(ns_prefixes.keys()) + r')\b')
+
     instances = h.get_class_instances(graph, cl)
     for i in instances:
-        po_items.append(["{p}{sep}{o}".format(p=str(p), o=str(o), sep=_predicate_object_separator)
-                        for (p, o) in graph.predicate_objects(i)])
+        pos = []
+        for (p, o) in graph.predicate_objects(i):
+            if ns_prefixes:
+                p = pattern.sub(lambda x: ns_prefixes[x.group()], p)
+                o = pattern.sub(lambda x: ns_prefixes[x.group()], o)
+            pos.append("{p}{sep}{o}".format(p=str(p), o=str(o), sep=_predicate_object_separator))
+        po_items.append(pos)
 
     # return po_items
 
@@ -87,6 +72,7 @@ def freq_items_by_class(graph, cl, minsup1=50, minsup2=25, minconf=90, minlift=2
     if return_code:
         print(return_code)
 
+    # Parse frequent itemsets
     with open("{file}.freq_itemsets".format(file=basket_file), encoding='UTF-8', mode='r') as f:
         freq_itemsets = []
         for row in f.readlines():
@@ -94,15 +80,15 @@ def freq_items_by_class(graph, cl, minsup1=50, minsup2=25, minconf=90, minlift=2
             support = float(row.split()[-1])
             freq_itemsets += [(pred_objs, support)]
 
+    # Parse frequent rules
     with open("{file}.freq_rules".format(file=basket_file), encoding='UTF-8', mode='r') as f:
         freq_rules = []
         for row in f.readlines():
             row_parts = row.split()
             supp, conf, lift = (float(part) for part in row_parts[-1].split(','))
-            antecedents_string, consequents_string = ' '.join(row_parts[:-1]).split(' <- ')
+            consequents_string, antecedents_string = ' '.join(row_parts[:-1]).split(' <- ')
             ante = [po.split(_predicate_object_separator) for po in antecedents_string.split()]
             cons = [po.split(_predicate_object_separator) for po in consequents_string.split()]
-            print('Ante: %s, \t Cons: %s' % (ante, cons))
             # freq_rules += [(ante, cons, supp, conf, lift)]
             freq_rules += [{'antecedents': ante,
                             'consequents': cons,
